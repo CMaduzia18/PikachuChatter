7b70d96fd954299f89c538a79d678134e944b344
CMaduzia18 <windward_student@school.windwardschool.org>
2016-09-07 09:41:11 -0700

    class

---
+++ src/PikachuChatter.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+
+
+public class PikachuChatter {
+	public static void pikachu(){
+		System.out.println("(\\__/)");
+		System.out.println("(o^.^)");
+		System.out.println("z(_(\")(\")");
+		System.out.println();
+	}
+	public static void main(String[] args){
+		System.out.println("\tPika pika pika chu pika chu peeeeeee ka pika chu!");
+		pikachu();
+		System.out.println("\tPika? Pika pika pika chu peeeeee ka chu!");
+		pikachu();
+	}
+}
